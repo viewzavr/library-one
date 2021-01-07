d4c91549631fff5f7d2885b67a433b612284b3d9
Pavel Vasev <pavel.vasev@gmail.com>
2021-01-07 22:26:48 +0500

    Update init.js

--- init.js
+++ init.js
@@ -1,22 +1,37 @@
+// insert here links to modules and libraries
+
 var arr = [
   "https://cdn.jsdelivr.net/gh/pavelvasev/vr-points-game@master/krako.js",
   "https://cdn.jsdelivr.net/gh/pavelvasev/vr-points-game@master/points-game/init.js"
 ];
+
+//////////////////////////////
+// our setup function here loads things by import(..) because
+// statical import will break an app on any error.
+
+function load( src ) {
+  var p = import( src );
+
+  p.then( function(module) {
+    // console.log("library-one: module loaded, calling it's setup",src );
+    module.default( vz );
+  });
+  // todo: create new promise
+
+  return p;
+}
 
 export default function setup(vz) {
   var promises = [];
 
   for (var i=0; i<arr.length; i++) {
     try {
-      var p = import( arr[i] );
-      p.then( function(module) {
-        console.log("library-one: module loaded, calling it's setup",arr[i]);
-        module.default( vz );
-      });
+      promises.push( load( arr[i] ) );
     } catch (err) {
       console.error("library-one: failed to import",arr[i],err );
     }
   }
-  
+
   return Promise.allSettled( promises );
 }
+
